b8b4ae950112e09acd4fc7aa040f7da72650daca
Albin Mema <albinmema25@gmail.com>
2025-07-01 22:47:41 +0200

    feat: initial Elixir/Phoenix project setup with Ash framework
    - Add mix.exs with Ash framework dependencies
    - Include Phoenix, Ash JSON API, Ash Admin, and OpenAPI Spex
    - Configure PostgreSQL database support
    - Set up development and testing dependencies

---
+++ riva_ash/mix.exs
@@ -0,0 +1,54 @@
+defmodule RivaAsh.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :riva_ash,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {RivaAsh.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ash, "~> 3.0"},
+      {:ash_postgres, "~> 2.0"},
+      {:ash_json_api, "~> 1.0"},
+      {:phoenix, "~> 1.7.0"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.6"},
+      {:postgrex, ">= 0.0.0"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"},
+      {:gettext, "~> 0.20"},
+      {:jason, "~> 1.2"},
+      {:dns_cluster, "~> 0.1.1"},
+      {:bandit, "~> 1.0"},
+      {:open_api_spex, "~> 3.5"},
+      {:cors_plug, "~> 3.0"},
+      {:finch, "~> 0.13"},
+      {:ash_admin, "~> 0.11"}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get", "ecto.setup"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+    ]
+  end
+end
