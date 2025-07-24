2eabffe2e8118334028960de42c390369435854d
Albin Mema <albinmema25@gmail.com>
2025-07-24 15:50:20 +0200

    fix: Pass actor to LiveView Ash operations

--- packages/riva_ash/lib/riva_ash_web/live/client_live.ex
+++ packages/riva_ash/lib/riva_ash_web/live/client_live.ex
@@ -6,21 +6,27 @@ defmodule RivaAshWeb.ClientLive do
 
   import RivaAshWeb.Components.Organisms.PageHeader
   import RivaAshWeb.Components.Organisms.DataTable
-  import RivaAshWeb.Components.Atoms.AllAtoms
+  import RivaAshWeb.Components.Atoms.Button
 
   alias RivaAsh.Resources.Client
 
   @impl true
-  def mount(_params, _session, socket) do
-    clients = RivaAsh.read(Client)
+  def mount(_params, session, socket) do
+    case get_current_user_from_session(session) do
+      {:ok, user} ->
+        clients = Client.read!(actor: user)
 
-    socket =
-      socket
-      |> assign(:page_title, "Clients")
-      |> assign(:clients, clients)
-      |> assign(:meta, %{}) # Placeholder for pagination/metadata
+        socket =
+          socket
+          |> assign(:current_user, user)
+          |> assign(:page_title, "Clients")
+          |> assign(:clients, clients)
+          |> assign(:meta, %{}) # Placeholder for pagination/metadata
 
-    {:ok, socket}
+        {:ok, socket}
+      {:error, _} ->
+        {:ok, redirect(socket, to: "/sign-in")}
+    end
   end
 
   @impl true
@@ -77,5 +83,21 @@ defmodule RivaAshWeb.ClientLive do
 
   def handle_event(_event, _params, socket) do
     {:noreply, socket}
+  end
+
+  # Private helper functions
+  defp get_current_user_from_session(session) do
+    user_token = session["user_token"]
+
+    if user_token do
+      with {:ok, user_id} <- Phoenix.Token.verify(RivaAshWeb.Endpoint, "user_auth", user_token, max_age: 86_400) |> RivaAsh.ErrorHelpers.to_result(),
+           {:ok, user} <- Ash.get(RivaAsh.Accounts.User, user_id, domain: RivaAsh.Accounts) |> RivaAsh.ErrorHelpers.to_result() do
+        RivaAsh.ErrorHelpers.success(user)
+      else
+        _ -> RivaAsh.ErrorHelpers.failure(:not_authenticated)
+      end
+    else
+      RivaAsh.ErrorHelpers.failure(:not_authenticated)
+    end
   end
 end
