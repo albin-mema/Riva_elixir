e8938e3c938b3e4d561cce3c88f2e470b8fd15ff
Albin Mema <albinmema25@gmail.com>
2025-07-26 00:55:58 +0200

    Add JavaScript source files

---
+++ packages/riva_ash/assets/js/storybook.js
@@ -0,0 +1,17 @@
+// Storybook specific JavaScript
+// This file is used for Phoenix Storybook integration
+
+// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
+import "phoenix_html"
+// Establish Phoenix Socket and LiveView configuration.
+import {Socket} from "phoenix"
+import {LiveSocket} from "phoenix_live_view"
+
+let csrfToken = document.querySelector("meta[name='csrf-token']")?.getAttribute("content")
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+
+// connect if there are any LiveViews on the page
+liveSocket.connect()
+
+// expose liveSocket on window for web console debug logs
+window.liveSocket = liveSocket
