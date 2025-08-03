71b0ad513fae368eb00ee2d31be215f693dcfbd0
albin-mema <albinmema25@gmail.com>
2025-08-03 18:26:28 +0200

    Storybook: Simplify button stories

--- packages/riva_ash/storybook/atoms/button.story.exs
+++ packages/riva_ash/storybook/atoms/button.story.exs
@@ -1,97 +1,41 @@
 defmodule Storybook.Atoms.Button do
   use PhoenixStorybook.Story, :component
 
-  def function, do: &RivaAshWeb.Components.Atoms.Button.button/1
+  def component, do: &RivaAshWeb.Components.Atoms.Button.button/1
+
+  def template do
+    """
+    <div class="sb-p-4">
+      <.button variant={@variant} {@rest}>
+        <%= @label %>
+      </.button>
+    </div>
+    """
+  end
 
   def variations do
     [
       %Variation{
-        id: :default,
-        attributes: %{},
-        slots: [
-          "Default Button"
-        ]
-      },
-      %Variation{
         id: :primary,
         attributes: %{
-          variant: "primary"
-        },
-        slots: [
-          "Primary Button"
-        ]
+          label: "Primary Button",
+          variant: :primary
+        }
       },
       %Variation{
         id: :secondary,
         attributes: %{
-          variant: "secondary"
-        },
-        slots: [
-          "Secondary Button"
-        ]
-      },
-      %Variation{
-        id: :destructive,
-        attributes: %{
-          variant: "destructive"
-        },
-        slots: [
-          "Destructive Button"
-        ]
-      },
-      %Variation{
-        id: :outline,
-        attributes: %{
-          variant: "outline"
-        },
-        slots: [
-          "Outline Button"
-        ]
-      },
-      %Variation{
-        id: :ghost,
-        attributes: %{
-          variant: "ghost"
-        },
-        slots: [
-          "Ghost Button"
-        ]
-      },
-      %Variation{
-        id: :link,
-        attributes: %{
-          variant: "link"
-        },
-        slots: [
-          "Link Button"
-        ]
-      },
-      %Variation{
-        id: :small,
-        attributes: %{
-          size: "sm"
-        },
-        slots: [
-          "Small Button"
-        ]
-      },
-      %Variation{
-        id: :large,
-        attributes: %{
-          size: "lg"
-        },
-        slots: [
-          "Large Button"
-        ]
+          label: "Secondary Button",
+          variant: :secondary
+        }
       },
       %Variation{
         id: :disabled,
         attributes: %{
+          label: "Disabled Button",
+          variant: :primary,
           disabled: true
-        },
-        slots: [
-          "Disabled Button"
-        ]
+        }
       }
     ]
   end
